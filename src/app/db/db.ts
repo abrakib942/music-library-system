859e998a17e9167064d5a90beffeb20969a1970f
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2024-01-16 12:01:23 +0600

    database created

---
+++ src/app/db/db.ts
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+  database: process.env.DB_NAME,
+});
+
+module.exports = pool;
